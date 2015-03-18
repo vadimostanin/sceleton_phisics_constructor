1d791cd0ca2b88677e546e1066794afa6b12f879
Vadym Ostanin <popcorn@bk.ru>
2015-03-18 20:37:20 +0200

    temove commented

--- src/DrawingContent.cpp
+++ src/DrawingContent.cpp
@@ -115,16 +115,10 @@ void DrawingContent::createDrawingCanvas()
 {
 	Evas * canvas = evas_object_evas_get( m_DrawingLayout );
 //	Evas_Object *r1 = elm_box_add( m_DrawingLayout );
-//
-//	evas_object_color_set(r1, 128, 128, 128, 255);
-//	evas_object_move(r1, 0, 50);
-//	evas_object_resize(r1, 800, 600);
-//	evas_object_show(r1);
 
 	Evas_Object * bx = elm_box_add( m_DrawingLayout );
 	evas_object_size_hint_align_set( bx, EVAS_HINT_FILL, EVAS_HINT_FILL );
 	evas_object_size_hint_weight_set( bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-//	elm_win_resize_object_add( m_MainWindowObject, bx);
 	elm_object_part_content_set( m_DrawingLayout, "drawing_canvas", bx );
 
 	Evas_Object * glview = elm_glview_add( m_DrawingLayout );
@@ -159,16 +153,6 @@ Evas_Object * DrawingContent::getDrawingCanvas()
 void DrawingContent::update()
 {
 	drawObjects();
-
-//	Eina_List * updates = evas_render_updates( getDrawingCanvas() );
-//	Eina_List * l = 0;
-//	Eina_Rectangle *rect;
-//
-//	EINA_LIST_FOREACH(updates, l, rect)
-//	{
-//		printf( "Rectangle (%d, %d, %d, %d) on canvas got a rendering update.\n", rect->x, rect->y,	rect->w, rect->h ); fflush( stdout );
-//	}
-//	evas_render_updates_free(updates);
 }
 
 void DrawingContent::setGraphicObjects( vector<IGraphicObject *> & graphicObjects )
@@ -206,28 +190,7 @@ void DrawingContent::clearObjects()
 	}
 	m_GraphicPoints.clear();
 }
-/*
-bool DrawingContent::getPoint( int x, int y, GraphicPoint & graphicPoint )
-{
-	vector<GraphicPoint>::iterator begin = m_GraphicPoints.begin();
-	vector<GraphicPoint>::iterator end = m_GraphicPoints.end();
-	vector<GraphicPoint>::iterator iter = begin;
-	for( ; iter != end ; iter++ )
-	{
-		int point_x = (*iter).getX();
-		int point_y = (*iter).getY();
 
-		if( x > ( point_x + SEARCHING_POINT_RADIUS ) || x < ( point_x - SEARCHING_POINT_RADIUS ) ||
-			y > ( point_y + SEARCHING_POINT_RADIUS ) || y < ( point_y - SEARCHING_POINT_RADIUS ) )
-		{
-			continue;
-		}
-		graphicPoint = (*iter);
-		return true;
-	}
-	return false;
-}
-*/
 
 
 
